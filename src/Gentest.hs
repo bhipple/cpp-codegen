2ca50bf5216e876fd6ca09c9634b76017e67cae6
Benjamin Hipple <bhipple@bloomberg.net>
2016-08-01 11:54:06 -0400

    Missing ; on namespace in test

--- src/Gentest.hs
+++ src/Gentest.hs
@@ -3,22 +3,23 @@ module Gentest ( genTestFile ) where
 import System.Environment (getArgs)
 import System.FilePath.Posix
 import Data.List (intersperse, intercalate, isPrefixOf)
+import Data.Monoid
 
 testName :: FilePath -> FilePath
-testName f = takeBaseName f ++ ".t.cpp"
+testName f = takeBaseName f <> ".t.cpp"
 
 commentLine :: FilePath -> String
-commentLine f = prefix ++ ws ++ tag
-    where prefix = "// " ++ testName f
-          tag = "-*-C++-*-"
+commentLine f = prefix <> ws <> tag
+    where prefix = "// " <> testName f
+          tag = "-*-C<>-*-"
           spaceCt = 79 - length prefix - length tag
           ws = replicate spaceCt ' '
 
 centeredTag :: String -> String
 centeredTag t = intercalate "\n" [separator, tagLn, separator]
-    where separator = "// " ++ replicate 76 '='
+    where separator = "// " <> replicate 76 '='
           spaceCt = 40 - (length t `div` 2) - 2
-          tagLn = "//" ++ replicate spaceCt ' ' ++ t
+          tagLn = "//" <> replicate spaceCt ' ' <> t
 
 namespaces :: [String] -> [String]
 namespaces lns = filter (/= "BloombergLP") namespaces
@@ -29,14 +30,14 @@ namespaces lns = filter (/= "BloombergLP") namespaces
 
 genTest :: FilePath -> [String] -> String
 genTest src namespaces = intercalate "\n" res
-    where nsUsings = map (\n -> "using namespace BloombergLP::" ++ n) namespaces
+    where nsUsings = map (\n -> "using namespace BloombergLP::" <> n <> ";") namespaces
           res = [commentLine src,
-                 "#include <" ++ takeBaseName src ++ ".h>\n",
+                 "#include <" <> takeBaseName src <> ".h>\n",
                  "// Application Includes\n",
                  "// System Includes",
                  "#include <gtest/gtest.h>\n",
                  "using namespace BloombergLP;"
-                ] ++ nsUsings ++
+                ] <> nsUsings <>
                 [ "", centeredTag "Tests", "\n"]
 
 genTestFile :: FilePath -> IO ()
@@ -44,9 +45,9 @@ genTestFile f = do
     contents <- readFile f
     let ns = namespaces (lines contents)
     let testContents = genTest f ns
-    let testName = dropExtension f ++ ".t.cpp"
+    let testName = dropExtension f <> ".t.cpp"
     writeFile testName testContents
-    putStrLn $ "Generated file " ++ testName ++ ":\n"
+    putStrLn $ "Generated file " <> testName <> ":\n"
     putStrLn testContents
 
 main = do
